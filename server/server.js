f778a70b251f1927bc651af8b02a06c3657ce132
Eric Niemeyer <niemeyer.eric@gmail.com>
2023-10-10 02:11:27 -0400

    js

--- server/server.js
+++ server/server.js
@@ -1,13 +1,8 @@
-// @ts-ignore
-import express from 'express';
+const express = require('express');
 const app = express();
-// @ts-ignore
 const server = require('http').Server(app);
-// @ts-ignore
 app.use(express.static(__dirname + '/public'));
-// @ts-ignore
 app.get('/', function (req, res) {
-  // @ts-ignore
   res.sendFile(__dirname + '/index.html');
 });
 server.listen(8081, function () {
