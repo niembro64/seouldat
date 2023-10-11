095cfbee76bc801c977eda026fa758a0c081e3d1
Eric Niemeyer <niemeyer.eric@gmail.com>
2023-10-11 01:04:57 -0400

    3

--- backend/server/authoritative_server/js/game.js
+++ backend/server/authoritative_server/js/game.js
@@ -22,11 +22,31 @@ const config = {
 
 function preload() {
   this.load.image('ship', 'assets/spaceShips_001.png');
+  this.load.image('star', 'assets/star_gold.png');
 }
 
 function create() {
   const self = this;
   this.players = this.physics.add.group();
+
+  this.scores = {
+    blue: 0,
+    red: 0
+  };
+
+  this.star = this.physics.add.image(randomPosition(700), randomPosition(500), 'star');
+  this.physics.add.collider(this.players);
+
+  this.physics.add.overlap(this.players, this.star, function (star, player) {
+    if (players[player.playerId].team === 'red') {
+      self.scores.red += 10;
+    } else {
+      self.scores.blue += 10;
+    }
+    self.star.setPosition(randomPosition(700), randomPosition(500));
+    io.emit('updateScore', self.scores);
+    io.emit('starLocation', { x: self.star.x, y: self.star.y });
+  });
 
   io.on('connection', function (socket) {
     console.log('a user connected');
@@ -36,7 +56,12 @@ function create() {
       x: Math.floor(Math.random() * 700) + 50,
       y: Math.floor(Math.random() * 500) + 50,
       playerId: socket.id,
-      team: (Math.floor(Math.random() * 2) == 0) ? 'red' : 'blue'
+      team: (Math.floor(Math.random() * 2) == 0) ? 'red' : 'blue',
+      input: {
+        left: false,
+        right: false,
+        up: false
+      }
     };
     // add player to server
     addPlayer(self, players[socket.id]);
@@ -44,6 +69,10 @@ function create() {
     socket.emit('currentPlayers', players);
     // update all other players of the new player
     socket.broadcast.emit('newPlayer', players[socket.id]);
+    // send the star object to the new player
+    socket.emit('starLocation', { x: self.star.x, y: self.star.y });
+    // send the current scores
+    socket.emit('updateScore', self.scores);
 
     socket.on('disconnect', function () {
       console.log('user disconnected');
@@ -54,10 +83,50 @@ function create() {
       // emit a message to all players to remove this player
       io.emit('disconnect', socket.id);
     });
+
+    // when a player moves, update the player data
+    socket.on('playerInput', function (inputData) {
+      handlePlayerInput(self, socket.id, inputData);
+    });
   });
 }
 
-function update() {}
+function update() {
+  this.players.getChildren().forEach((player) => {
+    const input = players[player.playerId].input;
+    if (input.left) {
+      player.setAngularVelocity(-300);
+    } else if (input.right) {
+      player.setAngularVelocity(300);
+    } else {
+      player.setAngularVelocity(0);
+    }
+
+    if (input.up) {
+      this.physics.velocityFromRotation(player.rotation + 1.5, 200, player.body.acceleration);
+    } else {
+      player.setAcceleration(0);
+    }
+
+    players[player.playerId].x = player.x;
+    players[player.playerId].y = player.y;
+    players[player.playerId].rotation = player.rotation;
+  });
+  this.physics.world.wrap(this.players, 5);
+  io.emit('playerUpdates', players);
+}
+
+function randomPosition(max) {
+  return Math.floor(Math.random() * max) + 50;
+}
+
+function handlePlayerInput(self, playerId, input) {
+  self.players.getChildren().forEach((player) => {
+    if (playerId === player.playerId) {
+      players[player.playerId].input = input;
+    }
+  });
+}
 
 function addPlayer(self, playerInfo) {
   const player = self.physics.add.image(playerInfo.x, playerInfo.y, 'ship').setOrigin(0.5, 0.5).setDisplaySize(53, 40);
