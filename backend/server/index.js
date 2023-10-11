4485b6d90b003a87addf8c58287ba511fefd2cd0
Eric Niemeyer <niemeyer.eric@gmail.com>
2023-10-11 00:53:14 -0400

    part 2

--- backend/server/index.js
+++ backend/server/index.js
@@ -3,7 +3,10 @@ const jsdom = require('jsdom');
 const express = require('express');
 const app = express();
 const server = require('http').Server(app);
+const io = require('socket.io').listen(server);
+const Datauri = require('datauri');
 
+const datauri = new Datauri();
 const { JSDOM } = jsdom;
 
 app.use(express.static(__dirname + '/public'));
@@ -21,11 +24,18 @@ function setupAuthoritativePhaser() {
     // So requestAnimatinFrame events fire
     pretendToBeVisual: true
   }).then((dom) => {
+    dom.window.URL.createObjectURL = (blob) => {
+      if (blob){
+        return datauri.format(blob.type, blob[Object.getOwnPropertySymbols(blob)[0]]._buffer).content;
+      }
+    };
+    dom.window.URL.revokeObjectURL = (objectURL) => {};
     dom.window.gameLoaded = () => {
-      server.listen(8081, function () {
+      server.listen(8082, function () {
         console.log(`Listening on ${server.address().port}`);
       });
     };
+    dom.window.io = io;
   }).catch((error) => {
     console.log(error.message);
   });
